3ab746b026bd260bb0d2b0a6d5976c50aafeb63d
des2yapath <divyanshahuja2017@gmail.com>
2025-09-23 22:16:20 +0530

    put triangle and mesh objects

--- camera.rs
+++ camera.rs
@@ -1,35 +1,70 @@
-extern crate nalgebra as na;
-
-use na::{Vec3};
+use crate::ray::Ray;
+use glam::DVec3;
+use rand::Rng;
 
 pub struct Camera {
-    campos: Vec3<f64>,
-    camdir: Vec3<f64>,
-    camright: Vec3<f64>,
-    camdown: Vec3<f64>,
+    origin: DVec3,
+    lower_left_corner: DVec3,
+    horizontal: DVec3,
+    vertical: DVec3,
+    u: DVec3,
+    v: DVec3,
+    lens_radius: f64,
 }
 
 impl Camera {
-    pub fn getCameraPosition(&self) -> Vec3<f64> { self.campos }
-    pub fn getCameraDirection(&self) -> Vec3<f64> { self.camdir }
-    pub fn getCameraRight(&self) -> Vec3<f64> { self.camright }
-    pub fn getCameraDown(&self) -> Vec3<f64> { self.camdown }
+    pub fn new(
+        lookfrom: DVec3,
+        lookat: DVec3,
+        vup: DVec3,
+        vfov: f64,
+        aspect_ratio: f64,
+        aperture: f64,
+        focus_dist: f64,
+    ) -> Self {
+        let theta = vfov.to_radians();
+        let h = (theta / 2.0).tan();
+        let viewport_height = 2.0 * h;
+        let viewport_width = aspect_ratio * viewport_height;
 
-    pub fn new(pos: Vec3<f64>, dir: Vec3<f64>, right: Vec3<f64>, down: Vec3<f64>) -> Self {
-        Camera {
-            campos: pos,
-            camdir: dir,
-            camright: right,
-            camdown: down,
+        let w = (lookfrom - lookat).normalize();
+        let u = vup.cross(w).normalize();
+        let v = w.cross(u);
+
+        let origin = lookfrom;
+        let horizontal = focus_dist * viewport_width * u;
+        let vertical = focus_dist * viewport_height * v;
+        let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - focus_dist * w;
+        let lens_radius = aperture / 2.0;
+
+        Self {
+            origin,
+            lower_left_corner,
+            horizontal,
+            vertical,
+            u,
+            v,
+            lens_radius,
         }
     }
 
-    pub fn default(&self) -> Self {
-        Camera::new(
-            Vec3::new(0.0, 0.0, 0.0),
-            Vec3::new(0.0, 0.0, 1.0),
-            Vec3::new(0.0, 0.0, 0.0),
-            Vec3::new(0.0, 0.0, 0.0),
+    pub fn get_ray(&self, s: f64, t: f64) -> Ray {
+        let rd = self.lens_radius * random_in_unit_disk();
+        let offset = self.u * rd.x + self.v * rd.y;
+
+        Ray::new(
+            self.origin + offset,
+            self.lower_left_corner + s * self.horizontal + t * self.vertical - self.origin - offset,
         )
+    }
+}
+
+fn random_in_unit_disk() -> DVec3 {
+    let mut rng = rand::thread_rng();
+    loop {
+        let p = DVec3::new(rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0), 0.0);
+        if p.length_squared() < 1.0 {
+            return p;
+        }
     }
 }
