ea3e0a22d63569d510b5cf62b0b0e175da2210bf
des2yapath <divyanshahuja2017@gmail.com>
2025-09-25 22:07:22 +0530

    Create ray.rs

---
+++ ray.rs
@@ -0,0 +1,17 @@
+use glam::DVec3;
+
+#[derive(Debug, Clone, Copy, Default)]
+pub struct Ray {
+    pub origin: DVec3,
+    pub direction: DVec3,
+}
+
+impl Ray {
+    pub fn new(origin: DVec3, direction: DVec3) -> Self {
+        Self { origin, direction }
+    }
+
+    pub fn at(&self, t: f64) -> DVec3 {
+        self.origin + t * self.direction
+    }
+}
